e0b53b52ad0edba7875fe558e45a426524f951de
Michele Azzolari <michele@azzolari.it>
2023-03-16 11:53:46 +0100

    add http_head and http_options functions. Add 'returnAlsoHeaders' option
    if returnAlsoHeaders is set to true, function will return an object
    with headers and body key

--- src/index.js
+++ src/index.js
@@ -7,11 +7,11 @@ const userAgent =
     ? `${npm_package_name}/${npm_package_version}`
     : `${packageJson.name}/${packageJson.version}`;
 
-const hasHeader = function (headers, name) {
-  return Object.keys(headers).filter(header => header.toLowerCase() === name).length > 0;
-};
+function hasHeader(headers, name) {
+  return Object.keys(headers).some(header => header.toLowerCase() === name);
+}
 
-const handleResponse = function (res) {
+function handleResponse(res) {
   const contentLength = res.headers.get('content-length');
   if (contentLength !== null && Number(contentLength) === 0) {
     return false;
@@ -21,9 +21,9 @@ const handleResponse = function (res) {
     return res.json();
   }
   return res.text();
-};
+}
 
-const handleHeaders = function (fetchOpts, headers) {
+function handleHeaders(fetchOpts, headers) {
   if (fetchOpts.headers) {
     Object.assign(fetchOpts.headers, headers);
   } else {
@@ -32,58 +32,132 @@ const handleHeaders = function (fetchOpts, headers) {
   if (!hasHeader(fetchOpts.headers, 'user-agent')) {
     fetchOpts.headers['User-Agent'] = userAgent;
   }
-};
+}
 
-const handlePayload = function (fetchOpts, data) {
-  const dataType = typeof data;
-  if (dataType !== 'undefined') {
-    if (dataType !== 'string') {
-      data = JSON.stringify(data);
-    }
-    if (!hasHeader(fetchOpts.headers, 'content-type')) {
-      fetchOpts.headers['Content-Type'] = 'application/json;charset=utf-8';
-    }
+function handlePayload(fetchOpts, data) {
+  if (data instanceof Buffer) {
     fetchOpts.body = data;
+    return;
   }
-};
+  const dataType = typeof data;
+  if (dataType !== 'string') {
+    data = JSON.stringify(data);
+  }
+  if (!hasHeader(fetchOpts.headers, 'content-type')) {
+    fetchOpts.headers['Content-Type'] = 'application/json;charset=utf-8';
+  }
+  fetchOpts.body = data;
+}
 
-const execute = function (method, path, data, headers = {}, fetchOpts = {}) {
+async function execute(method, path, data, reqHeaders = {}, fetchOpts = {}, returnAlsoHeaders = false) {
   fetchOpts.method = method;
 
-  handleHeaders(fetchOpts, headers);
+  handleHeaders(fetchOpts, reqHeaders);
 
-  handlePayload(fetchOpts, data);
+  data && handlePayload(fetchOpts, data);
 
-  return fetch(path, fetchOpts).then(res => {
-    if (res.status >= 400) {
-      const error = new Error(res.statusText);
-      error.http_code = res.status;
-      error.http_response = res;
-      throw error;
-    }
+  const res = await globalThis.fetch(path, fetchOpts);
+
+  if (res.status >= 400) {
+    const error = new Error(res.statusText);
+    error.http_code = res.status;
+    error.http_response = res;
+    throw error;
+  }
+  const { headers } = res;
+  if (method === 'HEAD') {
+    return { headers };
+  }
+  async function getBody() {
     if (res.status === 204) {
       return false;
     }
     return handleResponse(res);
-  });
-};
+  }
 
-export const http_get = (path, headers, fetchOpts) => {
-  return execute('GET', path, undefined, headers, fetchOpts);
-};
+  const body = await getBody();
 
-export const http_del = (path, data, headers, fetchOpts) => {
-  return execute('DELETE', path, data, headers, fetchOpts);
-};
+  function handleReturn() {
+    if (returnAlsoHeaders) {
+      return { headers, body };
+    }
+    return body;
+  }
+  return handleReturn();
+}
 
-export const http_put = (path, data, headers, fetchOpts) => {
-  return execute('PUT', path, data, headers, fetchOpts);
-};
+export async function http_head(path, headers, fetchOpts) {
+  return execute('HEAD', path, undefined, headers, fetchOpts);
+}
 
-export const http_post = (path, data, headers, fetchOpts) => {
-  return execute('POST', path, data, headers, fetchOpts);
-};
+export async function http_options(path, headers, fetchOpts, returnAlsoHeaders) {
+  if (typeof headers === 'boolean') {
+    returnAlsoHeaders = headers;
+    headers = undefined;
+    fetchOpts = undefined;
+  } else if (typeof fetchOpts === 'boolean') {
+    returnAlsoHeaders = fetchOpts;
+    fetchOpts = undefined;
+  }
+  return execute('OPTIONS', path, undefined, headers, fetchOpts, returnAlsoHeaders);
+}
 
-export const http_patch = (path, data, headers, fetchOpts) => {
-  return execute('PATCH', path, data, headers, fetchOpts);
-};
+export async function http_get(path, headers, fetchOpts, returnAlsoHeaders) {
+  if (typeof headers === 'boolean') {
+    returnAlsoHeaders = headers;
+    headers = undefined;
+    fetchOpts = undefined;
+  } else if (typeof fetchOpts === 'boolean') {
+    returnAlsoHeaders = fetchOpts;
+    fetchOpts = undefined;
+  }
+  return execute('GET', path, undefined, headers, fetchOpts, returnAlsoHeaders);
+}
+
+export async function http_del(path, data, headers, fetchOpts, returnAlsoHeaders) {
+  if (typeof headers === 'boolean') {
+    returnAlsoHeaders = headers;
+    headers = undefined;
+    fetchOpts = undefined;
+  } else if (typeof fetchOpts === 'boolean') {
+    returnAlsoHeaders = fetchOpts;
+    fetchOpts = undefined;
+  }
+  return execute('DELETE', path, data, headers, fetchOpts, returnAlsoHeaders);
+}
+
+export async function http_put(path, data, headers, fetchOpts, returnAlsoHeaders) {
+  if (typeof headers === 'boolean') {
+    returnAlsoHeaders = headers;
+    headers = undefined;
+    fetchOpts = undefined;
+  } else if (typeof fetchOpts === 'boolean') {
+    returnAlsoHeaders = fetchOpts;
+    fetchOpts = undefined;
+  }
+  return execute('PUT', path, data, headers, fetchOpts, returnAlsoHeaders);
+}
+
+export async function http_post(path, data, headers, fetchOpts, returnAlsoHeaders) {
+  if (typeof headers === 'boolean') {
+    returnAlsoHeaders = headers;
+    headers = undefined;
+    fetchOpts = undefined;
+  } else if (typeof fetchOpts === 'boolean') {
+    returnAlsoHeaders = fetchOpts;
+    fetchOpts = undefined;
+  }
+  return execute('POST', path, data, headers, fetchOpts, returnAlsoHeaders);
+}
+
+export async function http_patch(path, data, headers, fetchOpts, returnAlsoHeaders) {
+  if (typeof headers === 'boolean') {
+    returnAlsoHeaders = headers;
+    headers = undefined;
+    fetchOpts = undefined;
+  } else if (typeof fetchOpts === 'boolean') {
+    returnAlsoHeaders = fetchOpts;
+    fetchOpts = undefined;
+  }
+  return execute('PATCH', path, data, headers, fetchOpts, returnAlsoHeaders);
+}
---
+++ tests/e2e.test.js
@@ -0,0 +1,332 @@
+import { http_get, http_head, http_options, http_post, http_put, http_patch, http_del } from '../src';
+import express from 'express';
+import assert from 'assert';
+import { describe, it, before, after } from 'mocha';
+
+const app = express();
+
+const get_test = { get: true };
+
+const option_test = { options: true };
+
+const post_test = { post: true };
+
+const put_test = { put: true };
+
+const patch_test = { patch: true };
+
+const del_test = { del: true };
+
+const headerKey = 'x-test-header';
+const headerValue = 'fluidware srl';
+
+app.get('/', (req, res) => {
+  res.set(headerKey, headerValue).json(get_test);
+});
+
+app.head('/', (req, res) => {
+  res.set(headerKey, headerValue).status(200).send();
+});
+
+app.get('/headers', (req, res) => {
+  const header = req.get(headerKey);
+  if (!header || header !== headerValue) {
+    return res.status(400).json({ status: 400, reason: 'missing required header x-test-header' });
+  }
+  res.set(headerKey, headerValue).json(get_test);
+});
+
+app.get('/redirect', (req, res) => {
+  res.redirect('/');
+});
+
+app.options('/', (req, res) => {
+  res.set(headerKey, headerValue).json(option_test);
+});
+
+app.options('/headers', (req, res) => {
+  const header = req.get(headerKey);
+  if (!header || header !== headerValue) {
+    return res.status(400).json({ status: 400, reason: 'missing required header x-test-header' });
+  }
+  res.set(headerKey, headerValue).json(option_test);
+});
+
+app.options('/redirect', (req, res) => {
+  res.redirect('/');
+});
+
+app.post('/', express.text(), (req, res) => {
+  if (req.body !== 'test') {
+    return res.status(400).json({ status: 400, reason: 'missing required body' });
+  }
+  res.set(headerKey, headerValue).json(post_test);
+});
+
+app.post('/post', express.json(), (req, res) => {
+  try {
+    assert.deepStrictEqual(req.body, { test: true });
+  } catch (e) {
+    return res.status(400).json({ status: 400, reason: 'missing required body' });
+  }
+  res.set(headerKey, headerValue).json(post_test);
+});
+
+app.post('/post-empty', express.text(), express.json(), (req, res) => {
+  if (req.get('content-length') > 0) {
+    return res.status(400).json({ status: 400, reason: 'content-length greater than 0' });
+  }
+  // express.json() put {} event if no body is present..
+  try {
+    assert.deepStrictEqual(req.body, {});
+  } catch (e) {
+    return res.status(400).json({ status: 400, reason: 'body is present' });
+  }
+  res.set(headerKey, headerValue).json(post_test);
+});
+
+app.put('/', express.text(), (req, res) => {
+  if (req.body !== 'test') {
+    return res.status(400).json({ status: 400, reason: 'missing required body' });
+  }
+  res.set(headerKey, headerValue).json(put_test);
+});
+
+app.put('/put', express.json(), (req, res) => {
+  try {
+    assert.deepStrictEqual(req.body, { test: true });
+  } catch (e) {
+    return res.status(400).json({ status: 400, reason: 'missing required body' });
+  }
+  res.set(headerKey, headerValue).json(put_test);
+});
+
+app.patch('/', express.text(), (req, res) => {
+  if (req.body !== 'test') {
+    return res.status(400).json({ status: 400, reason: 'missing required body' });
+  }
+  res.set(headerKey, headerValue).json(patch_test);
+});
+
+app.patch('/patch', express.json(), (req, res) => {
+  try {
+    assert.deepStrictEqual(req.body, { test: true });
+  } catch (e) {
+    return res.status(400).json({ status: 400, reason: 'missing required body' });
+  }
+  res.set(headerKey, headerValue).json(patch_test);
+});
+
+app.delete('/', express.text(), (req, res) => {
+  if (req.body !== 'test') {
+    return res.status(400).json({ status: 400, reason: 'missing required body' });
+  }
+  res.set(headerKey, headerValue).json(del_test);
+});
+
+app.delete('/delete', express.json(), (req, res) => {
+  try {
+    assert.deepStrictEqual(req.body, { test: true });
+  } catch (e) {
+    return res.status(400).json({ status: 400, reason: 'missing required body' });
+  }
+  res.set(headerKey, headerValue).json(del_test);
+});
+
+describe('e2e', function () {
+  let port;
+  let server;
+  let url;
+  before(done => {
+    server = app.listen(0, '127.0.0.1', () => {
+      port = server.address().port;
+      url = `http://127.0.0.1:${port}`;
+      done();
+    });
+  });
+  after(done => {
+    server.closeAllConnections();
+    server.close(() => {
+      done();
+    });
+  });
+  describe('http_get', function () {
+    it('must return json body', async () => {
+      const ret = await http_get(`${url}/`);
+      assert.deepStrictEqual(ret, get_test);
+    });
+    it('must return json body following redirect', async () => {
+      const ret = await http_get(`${url}/redirect`);
+      assert.deepStrictEqual(ret, get_test);
+    });
+    it('must return nothing not following redirect', async () => {
+      const ret = await http_get(`${url}/redirect`, undefined, { redirect: 'manual' });
+      assert.deepStrictEqual(ret, 'Found. Redirecting to /');
+    });
+    it('must return json body sending custom header', async () => {
+      const ret = await http_get(`${url}/headers`, { [headerKey]: headerValue });
+      assert.deepStrictEqual(ret, get_test);
+    });
+    it('must return an object with body and response headers ', async () => {
+      const { body, headers } = await http_get(`${url}/`, true);
+      assert.deepStrictEqual(body, get_test);
+      assert.strictEqual(headers.get(headerKey), headerValue);
+    });
+    it('must return an object with body and response headers v2', async () => {
+      const { body, headers } = await http_get(`${url}/`, undefined, true);
+      assert.deepStrictEqual(body, get_test);
+      assert.strictEqual(headers.get(headerKey), headerValue);
+    });
+  });
+  describe('http_head', function () {
+    it('must return headers', async () => {
+      const { body, headers } = await http_head(`${url}/`);
+      assert.strictEqual(body, undefined);
+      assert.strictEqual(headers.get(headerKey), headerValue);
+    });
+    it('must return headers following redirect', async () => {
+      const { body, headers } = await http_head(`${url}/redirect`);
+      assert.strictEqual(body, undefined);
+      assert.strictEqual(headers.get('content-type'), 'application/json; charset=utf-8');
+    });
+    it('must return headers NOT following redirect', async () => {
+      const { body, headers } = await http_head(`${url}/redirect`, undefined, { redirect: 'manual' });
+      assert.strictEqual(body, undefined);
+      assert.strictEqual(headers.get('location'), '/');
+      assert.strictEqual(headers.get('content-type'), 'text/plain; charset=utf-8');
+    });
+  });
+  describe('http_options', function () {
+    it('must return json body', async () => {
+      const ret = await http_options(`${url}/`);
+      assert.deepStrictEqual(ret, option_test);
+    });
+    it('must return json body following redirect', async () => {
+      const ret = await http_options(`${url}/redirect`);
+      assert.deepStrictEqual(ret, option_test);
+    });
+    it('must return nothing not following redirect', async () => {
+      const ret = await http_options(`${url}/redirect`, undefined, { redirect: 'manual' });
+      assert.deepStrictEqual(ret, 'Found. Redirecting to /');
+    });
+    it('must return json body sending custom header', async () => {
+      const ret = await http_options(`${url}/headers`, { [headerKey]: headerValue });
+      assert.deepStrictEqual(ret, option_test);
+    });
+    it('must return an object with body and response headers ', async () => {
+      const { body, headers } = await http_options(`${url}/`, true);
+      assert.deepStrictEqual(body, option_test);
+      assert.strictEqual(headers.get(headerKey), headerValue);
+    });
+    it('must return an object with body and response headers v2', async () => {
+      const { body, headers } = await http_options(`${url}/`, undefined, true);
+      assert.deepStrictEqual(body, option_test);
+      assert.strictEqual(headers.get(headerKey), headerValue);
+    });
+  });
+  describe('http_post', function () {
+    it('must return json body posting Buffer', async () => {
+      const ret = await http_post(`${url}/`, Buffer.from('test', 'utf-8'), { 'content-type': 'text/plain' });
+      assert.deepStrictEqual(ret, post_test);
+    });
+    it('must return json body posting object', async () => {
+      const { body } = await http_post(`${url}/post`, { test: true }, true);
+      assert.deepStrictEqual(body, post_test);
+    });
+    it('must return json body', async () => {
+      const { body } = await http_post(`${url}/`, Buffer.from('test', 'utf-8'), { 'content-type': 'text/plain' }, true);
+      assert.deepStrictEqual(body, post_test);
+    });
+    it('must return json body - no payload', async () => {
+      const body = await http_post(`${url}/post-empty`);
+      assert.deepStrictEqual(body, post_test);
+    });
+    it('must return json body', async () => {
+      const { body } = await http_post(
+        `${url}/`,
+        Buffer.from('test', 'utf-8'),
+        { 'content-type': 'text/plain' },
+        undefined,
+        true
+      );
+      assert.deepStrictEqual(body, post_test);
+    });
+  });
+  describe('http_put', function () {
+    it('must return json body posting Buffer', async () => {
+      const ret = await http_put(`${url}/`, Buffer.from('test', 'utf-8'), { 'content-type': 'text/plain' });
+      assert.deepStrictEqual(ret, put_test);
+    });
+    it('must return json body posting object', async () => {
+      const { body } = await http_put(`${url}/put`, { test: true }, true);
+      assert.deepStrictEqual(body, put_test);
+    });
+    it('must return json body', async () => {
+      const { body } = await http_put(`${url}/`, Buffer.from('test', 'utf-8'), { 'content-type': 'text/plain' }, true);
+      assert.deepStrictEqual(body, put_test);
+    });
+    it('must return json body', async () => {
+      const { body } = await http_put(
+        `${url}/`,
+        Buffer.from('test', 'utf-8'),
+        { 'content-type': 'text/plain' },
+        undefined,
+        true
+      );
+      assert.deepStrictEqual(body, put_test);
+    });
+  });
+  describe('http_patch', function () {
+    it('must return json body posting Buffer', async () => {
+      const ret = await http_patch(`${url}/`, Buffer.from('test', 'utf-8'), { 'content-type': 'text/plain' });
+      assert.deepStrictEqual(ret, patch_test);
+    });
+    it('must return json body posting object', async () => {
+      const { body } = await http_patch(`${url}/patch`, { test: true }, true);
+      assert.deepStrictEqual(body, patch_test);
+    });
+    it('must return json body', async () => {
+      const { body } = await http_patch(
+        `${url}/`,
+        Buffer.from('test', 'utf-8'),
+        { 'content-type': 'text/plain' },
+        true
+      );
+      assert.deepStrictEqual(body, patch_test);
+    });
+    it('must return json body', async () => {
+      const { body } = await http_patch(
+        `${url}/`,
+        Buffer.from('test', 'utf-8'),
+        { 'content-type': 'text/plain' },
+        undefined,
+        true
+      );
+      assert.deepStrictEqual(body, patch_test);
+    });
+  });
+  describe('http_del', function () {
+    it('must return json body posting Buffer', async () => {
+      const ret = await http_del(`${url}/`, Buffer.from('test', 'utf-8'), { 'content-type': 'text/plain' });
+      assert.deepStrictEqual(ret, del_test);
+    });
+    it('must return json body posting object', async () => {
+      const { body } = await http_del(`${url}/delete`, { test: true }, true);
+      assert.deepStrictEqual(body, del_test);
+    });
+    it('must return json body', async () => {
+      const { body } = await http_del(`${url}/`, Buffer.from('test', 'utf-8'), { 'content-type': 'text/plain' }, true);
+      assert.deepStrictEqual(body, del_test);
+    });
+    it('must return json body', async () => {
+      const { body } = await http_del(
+        `${url}/`,
+        Buffer.from('test', 'utf-8'),
+        { 'content-type': 'text/plain' },
+        undefined,
+        true
+      );
+      assert.deepStrictEqual(body, del_test);
+    });
+  });
+});
